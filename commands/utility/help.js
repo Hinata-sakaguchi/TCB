84f6d200d2cd6e9fb8c80d0e6f68aa1429c87e29
Hinata-sakaguchi <netawakuryusei@gmail.com>
2024-01-02 19:25:45 +0900

    add help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,17 +1,57 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, EmbedBuilder,ActionRowBuilder } = require('discord.js');
+
 
 const helpEmbed = new EmbedBuilder()
     .setColor(0x7adeff)
     .setTitle('TCB')
-    .setDescription('# **ヘルプメニュー**\nオプションを選択してください。\n**Command Category**\nコマンドのカテゴリーを表示する。\n**Command Usage**\nコマンドの使用方法を表示する。\n**Help Hub**\nヘルプメニューの操作。')
-	.setImage('https://media1.tenor.com/m/dYif7hEuFLMAAAAC/ace-attorney-phoenix-wright.gif')
-	.setFooter({ text: 'TCB-help', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+    .setDescription('**ヘルプメニュー**\nオプションを選択してください。\n\n**Command Category**\nコマンドのカテゴリーを表示する。\n\n**Command Usage**\nコマンドの使用方法を表示する。\n\n**Help Hub**\nヘルプメニューの操作。')
+	.setImage('https://cdn.discordapp.com/attachments/1160486351091798066/1191670028781752360/353aa9c6e4b8f6ec.gif?ex=65a64833&is=6593d333&hm=88b665ab4733ff49618f77c3a3fc850c1a61879b1d54226731454c03be14465e&')
+	.setFooter({ text: 'TCB-help' });
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('tcb-help')
 		.setDescription('ヘルプを表示します。'),
+		
 	async execute(interaction) {
-		await interaction.reply({ embeds: [helpEmbed] });;
+		const CommandList = new StringSelectMenuBuilder()
+		.setCustomId('Command-List')
+		.setPlaceholder('Command List')
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('1.サーバー管理')
+				.setDescription('1ページ目: サーバー管理に関するコマンド一覧')
+				.setEmoji('1191619830432927764')
+				.setValue('server'),
+		);
+		
+		const CommandUsage = new StringSelectMenuBuilder()
+		.setCustomId('Command-Usage')
+		.setPlaceholder('Command Usage')
+		.setDisabled(true)
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('1.サーバー管理')
+				.setDescription('1ページ目: サーバー管理に関するコマンド一覧')
+				.setValue('server'),
+		);
+		const Operation = new StringSelectMenuBuilder()
+		.setCustomId('operation')
+		.setPlaceholder('Operation')
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('ホームに戻る')
+				.setEmoji('1191650658089177128')
+				.setValue('home'),
+		);
+		const row = new ActionRowBuilder()
+			.addComponents(CommandList);
+		const row2 = new ActionRowBuilder()
+			.addComponents(CommandUsage);
+		const row3 = new ActionRowBuilder()
+			.addComponents(Operation);
+
+		await interaction.reply({ embeds: [helpEmbed], components: [row, row2, row3], ephemeral: true  });
 	},
 };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const fs = require('node:fs');
 const path = require('node:path');
-
 const 
 {
     Client,
     Events,
     GatewayIntentBits,
-    Collection
+    Collection,
+	EmbedBuilder,
+	StringSelectMenuBuilder, 
+	StringSelectMenuOptionBuilder,
+	ActionRowBuilder 
 
 } = require("discord.js");
 
@@ -40,22 +43,231 @@ for (const folder of commandFolders) {
 }
 
 client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+	const currentDate = new Date();
+	const hours = currentDate.getHours().toString().padStart(2, '0');
+	const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+	const timeString = `${hours}:${minutes}`;
+
+	const helpEmbed = new EmbedBuilder()
+    .setColor(0x7adeff)
+    .setTitle('TCB')
+    .setDescription('**ヘルプメニュー**\nオプションを選択してください。\n\n**Command Category**\nコマンドのカテゴリーを表示する。\n\n**Command Usage**\nコマンドの使用方法を表示する。\n\n**Help Hub**\nヘルプメニューの操作。')
+	.setImage('https://cdn.discordapp.com/attachments/1160486351091798066/1191670028781752360/353aa9c6e4b8f6ec.gif?ex=65a64833&is=6593d333&hm=88b665ab4733ff49618f77c3a3fc850c1a61879b1d54226731454c03be14465e&')
+	.setFooter({ text: 'TCB-help' });
+
+	const serverEmbed = new EmbedBuilder()
+	.setColor(0x7adeff)
+	.setTitle('サーバー管理')
+	.setDescription('```addrole, clearmessage, createbutton, createchannel, createembed, createfreechannel, senddirectmessage```')
+	.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`});
+
+
+	const CommandList1 = new StringSelectMenuBuilder()
+		.setCustomId('Command-List')
+		.setPlaceholder('Command List')
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('1.サーバー管理')
+				.setDescription('1ページ目: サーバー管理に関するコマンド一覧')
+				.setEmoji('1191619830432927764')
+				.setValue('server'),
+		);
+	const CommandUsage1 = new StringSelectMenuBuilder()
+		.setCustomId('Command-Usage')
+		.setPlaceholder('Command Usage')
+		.setDisabled(false)
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/addrole')
+				.setDescription('指定したユーザーにロールを付与する。')
+				.setValue('addrole'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/clearmessage')
+				.setDescription('指定した数のメッセージを消す。')
+				.setValue('clearmessage'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/createbutton')
+				.setDescription('ボタンを作成する。')
+				.setValue('createbutton'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/createembed')
+				.setDescription('埋め込みを作成する。')
+				.setValue('createembed'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/createfreechannel')
+				.setDescription('フリーチャンネル作成パネルを作成する。')
+				.setValue('createfreechannel'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('/senddirectmessage')
+				.setDescription('指定したユーザーにダイレクトメッセージを送信する。')
+				.setValue('senddm'),
+		);
+
+	const Operation1 = new StringSelectMenuBuilder()
+		.setCustomId('operation')
+		.setPlaceholder('Operation')
+		.addOptions(
+			new StringSelectMenuOptionBuilder()
+				.setLabel('ホームに戻る')
+				.setEmoji('1191650658089177128')
+				.setValue('home'),
+		);
+	const row = new ActionRowBuilder()
+		.addComponents(CommandList1);
+		
+	const row2 = new ActionRowBuilder()
+		.addComponents(CommandUsage1);
+
+	const row3 = new ActionRowBuilder()
+		.addComponents(Operation1);
+
+		
+	if (interaction.isChatInputCommand())
+	{
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
 	}
+	else if(interaction.isAnySelectMenu)
+	{
+		if (interaction.customId === 'Command-List') 
+		{
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.update({ embeds: [serverEmbed], components: [row, row2, row3] });
+		}
+		else if(interaction.customId === "Command-Usage")
+		{
+			let value = interaction.values[0];
+
+			if (value === 'addrole')
+			{	
+				const addroleEmbed = new EmbedBuilder()
+					.setColor(0x7adeff)
+					.setTitle('addrole')
+					.setDescription('```/addrole, user:@User role:@role```')
+					.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+					.addFields(
+						{ name: '概要', value: '指定した人にロールを付与する。', inline: true },
+						{ name: '使い方', value: '```/addrole\nuser:@Tom\nrole:@Member```', inline: true },
+						{ name: 'コマンド', value: '/addrole', inline: true }
+					)
+					.addFields(
+						{ name:'必要な権限',value: 'User:ロールの管理\nBot:管理者', inline: true}
+					);
+
+				await interaction.update({ embeds: [addroleEmbed]});
+			}
+			else if (value === 'clearmessage')
+			{
+				const clearmessageEmbed = new EmbedBuilder()
+					.setColor(0x7adeff)
+					.setTitle('clearmassage')
+					.setDescription('```/clear, amount: 1-100```')
+					.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+					.addFields(
+						{ name: '概要', value: '指定した数だけメッセージを消す。', inline: true },
+						{ name: '使い方', value: '```/clearmessage amount: 2```', inline: true },
+						{ name: 'コマンド', value: '/clearmessage', inline: true }
+					)
+					.addFields(
+						{ name:'必要な権限',value: 'User:メッセージの管理\nBot:メッセージの管理', inline: true}
+					);
+
+				await interaction.update({ embeds: [clearmessageEmbed]});
+			}
+			else if (value === 'createbutton')
+			{
+				const createButtonEmbed = new EmbedBuilder()
+					.setColor(0x7adeff)
+					.setTitle('createbutton')
+					.setDescription('```/createbutton```')
+					.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+					.addFields(
+						{ name: '概要', value: 'ボタンを作成する。', inline: true },
+						{ name: '使い方', value: '```/createbutton```', inline: true },
+						{ name: '使えるモデル', value: 'URL:http, https', inline: true }
+					)
+					.addFields(
+						{ name:'コマンド',value: '/createbutton', inline: true},
+						{ name:'必要な権限',value: 'User:無し\nBot:無し', inline: true}
+					);
+
+				await interaction.update({ embeds: [createButtonEmbed]});
+			}
+			else if (value === 'createembed')
+			{
+				const createembedEmbed = new EmbedBuilder()
+				.setColor(0x7adeff)
+				.setTitle('createembed')
+				.setDescription('```/createembed```')
+				.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+				.addFields(
+					{ name: '概要', value: '埋め込みを作成する。', inline: true },
+					{ name: '使い方', value: '```/createembed```', inline: true },
+					{ name:'コマンド',value: '/createbutton', inline: true}
+				)
+				.addFields(
+					{ name:'必要な権限',value: 'User:無し\nBot:無し', inline: true}
+				);
+
+				await interaction.update({ embeds: [createembedEmbed]});
+			}
+			else if (value === 'createfreechannel')
+			{
+				const createfreechannelEmbed = new EmbedBuilder()
+				.setColor(0x7adeff)
+				.setTitle('createfreechannel')
+				.setDescription('```/createfreechannel```')
+				.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+				.addFields(
+					{ name: '概要', value: 'フリーチャンネル作成パネルを作成する。', inline: true },
+					{ name: '使い方', value: '```/createfreechannel```', inline: true },
+					{ name:'コマンド',value: '/createfreechannel', inline: true}
+				)
+				.addFields(
+					{ name:'必要な権限',value: 'User:管理者\nBot:無し', inline: true}
+				);
+
+				await interaction.update({ embeds: [createfreechannelEmbed]});
+			}
+			else if (value === 'senddm')
+			{
+				const senddirectmessageEmbed = new EmbedBuilder()
+				.setColor(0x7adeff)
+				.setTitle('senddirectmessage')
+				.setDescription('```/senddirectmessage user:@user text:TEXT```')
+				.setFooter({ text: `1/8 ページ, サーバー管理コマンド・${timeString}`})
+				.addFields(
+					{ name: '概要', value: '指定した人にダイレクトメッセージを送る。', inline: true },
+					{ name: '使い方', value: '```/dm \nuser: @Noa\ntext:Hello```', inline: true },
+					{ name:'コマンド',value: '/senddirectmessage', inline: true}
+				)
+				.addFields(
+					{ name:'必要な権限',value: 'User:管理者\nBot:無し', inline: true}
+				);
+
+				await interaction.update({ embeds: [senddirectmessageEmbed]});
+			}
+		}
+		else if (interaction.customId === 'operation')
+		{
+			CommandUsage1.setDisabled(true)
+			await interaction.update({ embeds: [helpEmbed], components: [row, row2, row3] });
 		}
 	}
 });
